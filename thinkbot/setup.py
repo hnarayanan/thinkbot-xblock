0b6bf2f7e0bd89e3da4b6debe650f136e5b394b7
Harish Narayanan <hnarayanan@gmail.com>
2013-07-08 20:56:57 +0200

    Reflect renaming of the xblock in the setup file

--- thinkbot/setup.py
+++ thinkbot/setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='thinkbot-xblock',
-    version='0.1',
+    version='0.2',
     description='Thinkbot XBlock',
     py_modules=['thinkbot'],
     install_requires=['XBlock'],
     entry_points={
         'xblock.v1': [
-            'thinkbot = thinkbot:ThinkbotSimulation',
+            'thinkbot = thinkbot:ThinkbotSolutionBlock',
         ]
     }
 )
